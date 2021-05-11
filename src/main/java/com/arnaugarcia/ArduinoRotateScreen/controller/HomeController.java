35713754e7363671af89b103634504124f965fc1
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-05-11 14:53:03 +0200

    Set scroll pane background to white

--- src/main/java/com/arnaugarcia/ArduinoRotateScreen/controller/HomeController.java
+++ src/main/java/com/arnaugarcia/ArduinoRotateScreen/controller/HomeController.java
@@ -1,20 +1,15 @@
 package com.arnaugarcia.ArduinoRotateScreen.controller;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
 
-    @FXML
-    public Label label;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Init Home controller");
-        label.setText("Hello from Home Controller");
     }
 }
--- src/main/java/com/arnaugarcia/ArduinoRotateScreen/controller/MainController.java
+++ src/main/java/com/arnaugarcia/ArduinoRotateScreen/controller/MainController.java
@@ -1,19 +1,14 @@
 package com.arnaugarcia.ArduinoRotateScreen.controller;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import static java.util.Objects.requireNonNull;
 import static javafx.fxml.FXMLLoader.load;
 
 public class MainController implements Initializable {
