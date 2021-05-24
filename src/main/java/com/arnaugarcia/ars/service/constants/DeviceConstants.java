36162c971a5d3099412796e538a5b6cf0de0e3f5
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-05-24 20:40:02 +0200

    Updated delimiters

--- src/main/java/com/arnaugarcia/ars/service/constants/DeviceConstants.java
+++ src/main/java/com/arnaugarcia/ars/service/constants/DeviceConstants.java
@@ -1,6 +1,6 @@
 package com.arnaugarcia.ars.service.constants;
 
 public class DeviceConstants {
-    public static final String PIPE_AND_NEW_LINE = "[|&\\r\\n]";
+    public static final String PIPE_AND_HASHTAG = "[|&\\#]";
 
 }
