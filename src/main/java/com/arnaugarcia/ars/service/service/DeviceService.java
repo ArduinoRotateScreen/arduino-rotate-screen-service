894c02a4bd3f11919c649717d84214649ad4d80a
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-05-24 18:38:40 +0200

    Added method for attach device listener

--- src/main/java/com/arnaugarcia/ars/service/service/DeviceService.java
+++ src/main/java/com/arnaugarcia/ars/service/service/DeviceService.java
@@ -1,9 +1,12 @@
 package com.arnaugarcia.ars.service.service;
 
 import com.arnaugarcia.ars.service.domain.Device;
+import com.fazecast.jSerialComm.SerialPortDataListener;
 
 import java.util.List;
 
 public interface DeviceService {
     List<Device> getDeviceList();
+
+    void attachListener(Device device, SerialPortDataListener listener);
 }
