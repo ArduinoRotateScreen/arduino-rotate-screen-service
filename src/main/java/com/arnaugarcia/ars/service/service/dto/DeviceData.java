60f8898194772dd47a602d3d7d2b0a2fa4c04540
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-05-24 20:41:52 +0200

    Fixed to String and updated delimiter

--- src/main/java/com/arnaugarcia/ars/service/service/dto/DeviceData.java
+++ src/main/java/com/arnaugarcia/ars/service/service/dto/DeviceData.java
@@ -2,7 +2,7 @@ package com.arnaugarcia.ars.service.service.dto;
 
 import lombok.Getter;
 
-import static com.arnaugarcia.ars.service.constants.DeviceConstants.PIPE_AND_NEW_LINE;
+import static com.arnaugarcia.ars.service.constants.DeviceConstants.PIPE_AND_HASHTAG;
 import static java.lang.Double.parseDouble;
 import static org.springframework.util.ObjectUtils.isEmpty;
 
@@ -11,11 +11,16 @@ public class DeviceData {
     private Double pitch;
     private Double roll;
 
-    public DeviceData(String data) {
-        String[] result = data.split(PIPE_AND_NEW_LINE);
+    public DeviceData(String data)  {
+        String[] result = data.split(PIPE_AND_HASHTAG);
         if (!isEmpty(result)) {
-          this.pitch = parseDouble(result[0]);
-          this.roll = parseDouble(result[1]);
+            this.pitch = parseDouble(result[0]);
+            this.roll = parseDouble(result[1]);
         }
+    }
+
+    @Override
+    public String toString() {
+        return pitch + "|" + roll + "\n";
     }
 }
