1f787dddd0e08babd768a44b6982208d823268a1
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-06-12 19:30:54 +0200

    Transform device data to DisplayOrientation

--- src/main/java/com/arnaugarcia/ars/service/service/dto/DeviceData.java
+++ src/main/java/com/arnaugarcia/ars/service/service/dto/DeviceData.java
@@ -4,16 +4,22 @@ import com.arnaugarcia.ars.service.domain.DisplayOrientation;
 import com.arnaugarcia.ars.service.service.exception.DeviceDataException;
 import lombok.Getter;
 
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+
 import static com.arnaugarcia.ars.service.constants.DeviceConstants.PIPE_AND_HASHTAG;
 import static java.lang.Double.parseDouble;
+import static java.lang.Math.abs;
+import static java.util.Arrays.stream;
 import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Getter
 public class DeviceData {
-    private Double pitch;
-    private Double roll;
+    private double pitch;
+    private double roll;
 
-    public DeviceData(String data)  {
+    public DeviceData(String data) {
         String[] result = data.split(PIPE_AND_HASHTAG);
         try {
             if (!isEmpty(result)) {
@@ -24,6 +30,30 @@ public class DeviceData {
             throw new DeviceDataException();
         }
 
+    }
+
+    public Optional<DisplayOrientation> getDisplayOrientation(int threshold) {
+        final int deviceValue = (int) (abs(roll));
+        return stream(DisplayOrientation.values())
+                .map(convertByThreshold(threshold, deviceValue))
+                .filter(Objects::nonNull)
+                .findFirst();
+    }
+
+    private Function<DisplayOrientation, DisplayOrientation> convertByThreshold(int threshold, int deviceValue) {
+        return displayOrientation -> {
+            final int maxOrientation = displayOrientation.getOrientation() + threshold;
+            final int minOrientation = displayOrientation.getOrientation() - threshold;
+            if (between(deviceValue, maxOrientation, minOrientation)) {
+                return displayOrientation;
+            } else {
+                return null;
+            }
+        };
+    }
+
+    private boolean between(int value, int max, int min) {
+        return value >= min && value <= max;
     }
 
     @Override
